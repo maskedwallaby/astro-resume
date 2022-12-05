2474222bd325fcf513bc6b85ac69517d174bff80
Chris Weiser <maskedwallaby@pm.me>
2022-12-05 01:30:36 +0000

    Added cards, fonts.

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere] in your site by using the `import` keyword.
+
+export const { site_title } = "My resume";
+export const SITE_DESCRIPTION = "Welcome to my website!";
---
+++ todo.md
@@ -0,0 +1,6 @@
+# Todo
+
+1. Include global styles
+2. Include basic.css thing
+3. Style layout (or 4)
+4. Set up section components (or 3)
